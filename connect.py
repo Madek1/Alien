b68e283c926d4d9c3b91d9126ef5812a06eb6d9d
Madek1 <michubilu@wp.pl>
2018-09-06 22:21:04 +0200

    Add connecting system

---
+++ connect.py
@@ -0,0 +1,22 @@
+import telnetlib
+import time
+
+# Connect with Alien
+telnet = telnetlib.Telnet("192.168.0.24", "23")
+
+# Name
+print(telnet.read_until(b"Username>"))
+telnet.write(b"alien\n")
+
+# Password
+print(telnet.read_until(b"Password>"))
+telnet.write(b"password\n")
+
+print(telnet.read_until(b"Alien>"))
+
+# Send command
+while True:
+    telnet.write(b"get TagList\n")
+    time.sleep(1)
+    print(telnet.read_until(b'Alien>'))
+    print("\n")
